53584e379b2fc8dbc57f7ba5a3bb6163f0244dd8
bhhoang818 <88428487+bhhoang818@users.noreply.github.com>
2022-03-01 18:07:57 +0700

    add

--- src/js/main.js
+++ src/js/main.js
@@ -77,14 +77,18 @@ const customerSelect = () => {
 const handleClickSlide = () => {
     var handleClick = $('.history-list .history-list_header');
     var notification = $('header').find('.notification');
-    console.log(notification);
     handleClick.on('click', () => {
         handleClick.toggleClass('active');
         handleClick.parent().find('.list-history').slideToggle();
     });
     notification.on('click', () => {
         $('header').find('.notification-wrapper').slideToggle();
+        $('#overlay').toggleClass('active');
     });
+    $('#overlay').on('click', () => {
+        $('#overlay').removeClass('active');
+        $('header').find('.notification-wrapper').slideUp();
+    })
 }
 
 const tabsPanel = () => {
@@ -104,6 +108,90 @@ const tabsPanel = () => {
         }
 
     });
+
+    var theTabs = $(".nav-tabs li");
+    var i;
+
+    function theTabClicks(tabClickEvent) {
+        var clickedTab = tabClickEvent.currentTarget;
+        var tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+        var theTabs = tabParent.querySelectorAll(".nav-tabs li");
+        for (var i = 0; i < theTabs.length; i++) {
+            theTabs[i].classList.remove("active");
+        }
+
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var contentPanes = tabParent.querySelectorAll(".panel");
+        for (i = 0; i < contentPanes.length; i++) {
+            contentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = tabParent.querySelector(activePaneId);
+        activePane.classList.add("active");
+
+    }
+    for (i = 0; i < theTabs.length; i++) {
+        theTabs[i].addEventListener("click", theTabClicks)
+    }
+}
+const myChart = () => {
+    new Chart("myChart1", {
+        type: "doughnut",
+        data: {
+            datasets: [{
+                data: [18, 64, 18],
+                backgroundColor: [
+                    '#EB2629',
+                    '#FF993B',
+                    '#FFC20E'
+                ],
+            }]
+        },
+        options: {
+            cutout: 65,
+            hoverOffset: 0,
+            borderWidth: 0
+        }
+    });
+    new Chart("myChart2", {
+        type: "doughnut",
+        data: {
+            datasets: [{
+                label: 'My First Dataset',
+                data: [18, 41, 41],
+                backgroundColor: [
+                    '#7DBA00',
+                    '#EB2629',
+                    '#FFC20E'
+                ],
+            }]
+        },
+        options: {
+            cutout: 65,
+            hoverOffset: 0,
+            borderWidth: 0
+        }
+    });
+}
+const swiperInit = () => {
+    var swiper = new Swiper(".gallery-thumbnail", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".gallery-top", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
 }
 $(document).ready(function () {
     startMarquee();
@@ -112,6 +200,8 @@ $(document).ready(function () {
     customerSelect();
     handleClickSlide();
     tabsPanel();
+    myChart();
+    swiperInit();
     $("#progressbar-individualCustomer").progressbar({
         value: 3
     });
